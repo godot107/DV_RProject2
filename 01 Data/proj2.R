2caf676836f28ace1f1b335a5e049a721591d7ec
Willie Man <man.willieman@yahoo.com>
2015-02-14 17:15:17 -0600

    no message

--- 01 Data/proj2.R
+++ 01 Data/proj2.R
@@ -1,2 +1,6 @@
-df <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from BABY"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_wkm285', PASS='orcl_wkm285', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
+baby <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from BABY"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_wkm285', PASS='orcl_wkm285', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
 
+head(baby)
+
+#PLOT 1
+ggplot(baby, aes(x=AGE, y=BWT)) + geom_point() +  facet_wrap(~RACE)
