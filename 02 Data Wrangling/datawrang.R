182b6ac9545b7f90dcf4b92c6e1db42c6a90aeeb
Willie Man <man.willieman@yahoo.com>
2015-02-15 19:56:38 -0600

    no message

--- 02 Data Wrangling/datawrang.R
+++ 02 Data Wrangling/datawrang.R
@@ -5,7 +5,14 @@ require(dplyr)
 
 baby <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from BABY"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_wkm285', PASS='orcl_wkm285', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
 
-baby %>% select(AGE, BWT) %>% tbl_df
 
-#BWT are in grams.
-baby %>% group_by(RACE) %>% summarise(mean = mean(BWT), n = n()) %>% ggplot(aes(x = RACE, y = mean)) + geom_point()
+#DATA WRANGLE 1
+baby %>% select(AGE, BWT, SMOKE) %>% tbl_df
+baby %>% select(AGE, BWT, SMOKE) %>% ggplot(aes(x = AGE, y = BWT)) + geom_point() + facet_wrap(~SMOKE)
+
+#BWT are in grams. trying to fix this one.
+diamonds %>% group_by(cut,color, clarity) %>% summarise(mean_carat = mean(carat)) #yields 4 columns
+
+baby %>% group_by (LOW,LWT,UI) %>% summarise (mean_bwt = mean(BWT))
+
+diamonds %>% select(cut, clarity) %>% filter(cut %in% c("Good", "Fair")) %>% tbl_df
