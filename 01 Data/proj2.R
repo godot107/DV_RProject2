2dcaa94d95d4b31c7bc9df622db4c85e2d6b2b40
Willie Man <man.willieman@yahoo.com>
2015-02-15 20:24:21 -0600

    no message

--- 01 Data/proj2.R
+++ 01 Data/proj2.R
@@ -1,5 +1,7 @@
 require(tidyr)
 require(dplyr)
+library("RCurl")
+library("ggplot2")
 
 baby <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from BABY"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_wkm285', PASS='orcl_wkm285', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
 
